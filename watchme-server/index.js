057cad512695c9f32ded2e14ffa918b8bd67a53c
CattyVayrex <vayrexam@gmail.com>
2025-04-16 21:58:59 +0330

    port changed

--- watchme-server/index.js
+++ watchme-server/index.js
@@ -265,5 +265,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5002;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
